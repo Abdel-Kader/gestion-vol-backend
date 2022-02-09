b78ffdf0bbbd57cffd94f68af812389b77f2de58
Abdel-Kader <kabdel552@gmail.com>
2022-02-09 21:29:20 +0000

    code refactoring

--- src/main/java/com/bizao/gestionvol/repositories/AirportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bizao.gestionvol.repositories;
-
-import com.bizao.gestionvol.entities.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirportRepository extends JpaRepository<Airport, Integer>
-{
-}
--- src/main/java/com/bizao/gestionvol/services/AirportService.java
+++
@@ -1,9 +0,0 @@
-package com.bizao.gestionvol.services;
-
-import com.bizao.gestionvol.dto.AirportDTO;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface AirportService extends AbstractService<AirportDTO>
-{
-}
